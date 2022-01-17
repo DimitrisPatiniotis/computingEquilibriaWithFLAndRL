7872d320a684e9fc492061c2e9336a91612af8d2
DimitrisPatiniotis <dimpatspy@gmail.com>
2022-01-17 05:30:24 +0200

    reinforcement-learning-zerosum

--- compute_equilibria.py
+++ compute_equilibria.py
@@ -1,6 +1,9 @@
+from random import random, choices
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import linprog
 import sys
+
 
 odd_even_player_1_payoff = np.array([
             [1, -1],
@@ -146,11 +149,176 @@ def fictitious_play():
 
 
 def reinforcement_learning():
-    print('Reinforcement Learning Not Yet Available')
+    try:
+        iteration = int(sys.argv[3])
+        if iteration <= 0:
+            print('Iteration nubmer is invalid, setting it to 10.000 (default)')
+            iteration = 10000
+    except:
+        print('Iteration nubmer is invalid, setting it to 10.000 (default)')
+        iteration = 10000
+
+    if sys.argv[2] == 'oddeven':
+        pay_matrix_player_1 = odd_even_player_1_payoff
+        game_type = 'zero_sum'
+    elif sys.argv[2] == 'attackersdefenders':
+        pay_matrix_player_1 = attackers_defenders_player_1_payoff
+        game_type = 'zero_sum'
+    elif sys.argv[2] == 'coordination':
+        pay_matrix_player_1 = coordination_player_1_payoff
+        game_type = 'coordination'
+    else:
+        print('Please choose a valid input')
+        print('Odd Even: oddeven')
+        print('Attackers Defenders: attackersdefenders')
+    
+    moves = np.array([0,1])
+    #Init Values PLAYER1
+    valueQ_p1 = np.array([[1.0,1.0],[1.0,1.0]])
+    valueV_p1 = 1.0
+    valueP_p1 = np.array([0.5,0.5])
+    #Init Values PLAYER2
+    valueQ_p2 = np.array([[1.0,1.0],[1.0,1.0]])
+    valueV_p2 = 1.0
+    valueP_p2 = np.array([0.5,0.5])
+
+    def Reward(action1,action2, player):
+        player1reward = pay_matrix_player_1[action1][action2]
+        if player == 1:
+            return player1reward
+        elif player == 2:
+            if player1reward < 0:
+                return(abs(player1reward))
+            else:
+                return 0-player1reward
+
+    def chooseMove(moves,weights):
+        return choices(moves,weights,k=1)[0]
+
+    def updateQvalues(a, reward, move,moveO, player):
+        if player == 1:
+            return (1-alpha)*valueQ_p1[move][moveO] + alpha*move
+        elif player == 2:
+            return (1-a)*valueQ_p2[move][moveO] + a*move
+
+    def solveLinearProgram(Q):
+        c = np.array([[0.0,0.0,1.0]])
+        b_ub = np.array([0.0,0.0])
+        b_eq = np.array([1.0])
+        A_eq = np.array([[1.0,1.0,0.0]])
+        x0_bounds = (0.0,1.0)
+        x1_bounds = (0.0,1.0)
+        x2_bounds = (-1.0, 1.0)
+        bounds = [x0_bounds, x1_bounds, x2_bounds]
+
+        test1 = []
+        test1.append(Q[0][0])
+        test1.append(Q[1][0])
+        test1.append(1)
+
+        test2 = []
+        test2.append(Q[0][1])
+        test2.append(Q[1][1])
+        test2.append(1)
+
+        #Inequality constrain matrix
+        A_ub = []
+        A_ub.append(test1)
+        A_ub.append(test2)
+
+        result = linprog(c,A_ub=A_ub,b_ub=b_ub,A_eq=A_eq,b_eq=b_eq,bounds=bounds)
+
+        return result.x[0],result.x[1]
+
+    def computeValue(policy,Q, player):
+        if player == 1:
+            move1 = policy[0]*Q[0][0] + policy[1]*Q[1][0]
+            move2 = policy[0]*Q[0][1] + policy[1]*Q[1][1]
+            return min(move1,move2)
+        elif player == 2:
+            move1 = policy[0]*Q[0][0] + policy[1]*Q[1][0]
+            move2 = policy[0]*Q[0][1] + policy[1]*Q[1][1]
+            return max(move1,move2)
+
+    def choose_random_or_policy(valueP):
+        if random() > exploration:
+            return valueP
+        else:
+            return np.array([0.5,0.5])
+
+    alpha = 1.0
+    exploration = 0.5
+    decay = 0.99
+
+    player1play =[]
+    player2play =[]
+    play1average =[]
+    play2average = []
+    totalpayoff1 = 0
+    totalpayoff2 = 0
+
+    for i in range(iteration):
+
+        # Next move randomness
+        weights1 = choose_random_or_policy(valueP_p1)
+        weights2 = choose_random_or_policy(valueP_p2)
+
+        # Get Player Move
+        move_p1 = chooseMove(moves,weights1)  
+        move_p2 = chooseMove(moves,weights2)
+
+        player1play.append(move_p1)
+        player2play.append(move_p2)
+        if i>0:
+            play1average.append(np.average(player1play))
+            play2average.append(np.average(player2play))
+
+        # Getting last move reward
+        rewardp1 = Reward(move_p1,move_p2, player = 1)
+        rewardp2 = Reward(move_p2,move_p1,  player = 2)
+
+        totalpayoff1 += rewardp1
+        totalpayoff2 += rewardp2
+
+        # Updating Q values
+        valueQ_p1[move_p1][move_p2] = updateQvalues(alpha,rewardp1,move_p1,move_p2, player = 1)
+        valueQ_p2[move_p2][move_p1] = updateQvalues(alpha,rewardp1,move_p2,move_p1, player = 2)
+
+        # Player Q values
+        player1_Qvalues = [valueQ_p1[0][0], valueQ_p1[1][0], valueQ_p1[0][1], valueQ_p1[1][1]]
+        player2_Qvalues = [valueQ_p2[0][0], valueQ_p2[1][0], valueQ_p2[0][1], valueQ_p2[1][1]]
+
+        # Policy Iteration
+        valueP_p1[0], valueP_p1[1] = solveLinearProgram(valueQ_p1)
+        valueP_p2[0], valueP_p2[1] = solveLinearProgram(valueQ_p2)
+
+        # Updating alpha
+        alpha = alpha*decay
+
+    # Displaying Results
+    player1strategy = valueP_p1
+    player2strategy = valueP_p2
+    print('After {} iterations we get:'.format(iteration))
+    print('Column player strategy:')
+    print(str(player1strategy) + '\nTotal column player payoff:\n' + str(totalpayoff1))
+    print('Row player')
+    print(str(player2strategy) + '\nTotal row player payoff:\n' + str(totalpayoff2))
+    x = np.arange(0, (iteration-1), 1)
+    if sys.argv[2] == 'oddeven':
+        plt.title('Odd Even with Reinforcement Learning')
+    elif sys.argv[2] == 'attackersdefenders':
+        plt.title('Attackers Defenders with Reinforcement Learning')
+    elif sys.argv[2] == 'coordination':
+        plt.title('Coordination with Reinforcement Learning')
+    plt.xlabel("Iterations")
+    plt.ylabel("Relative move frequency")
+    plt.plot(x, play1average, label='Column Player')
+    plt.plot(x, play2average, label='Row Player')
+    plt.legend()
+    plt.show()
 
 
 def main():
-    
     if sys.argv[1] == 'fp':
         if sys.argv[2] == 'coordination':
             try:
